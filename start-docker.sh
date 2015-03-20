77b7f693c6503c8d48d87afd2b99cf3b415144b5
oleewere <oleewere@gmail.com>
2015-03-20 12:18:29 +0100

    CLOUD-459 add binary docker support

---
+++ start-docker.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+build-cloudbreak() {
+    ./gradlew clean build
+}
+
+run-cloudbreak() {
+    if [ "$DEBUG" ]; then
+        params="-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005"
+    fi
+    java $params -jar build/libs/cloudbreak-*.jar
+}
+
+main() {
+  build-cloudbreak
+  run-cloudbreak
+}
+
+[[ "$0" == "$BASH_SOURCE" ]] && main "$@"
