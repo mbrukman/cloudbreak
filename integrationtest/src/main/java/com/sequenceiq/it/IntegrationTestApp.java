5145f41092af1ab6044f1e44aef4166285db5779
Ferenc Schneider <ferenc.schneider@sequenceiq.com>
2015-04-10 10:38:59 +0200

    CLOUD-517: Smoke tests

--- integrationtest/src/main/java/com/sequenceiq/it/IntegrationTestApp.java
+++ integrationtest/src/main/java/com/sequenceiq/it/IntegrationTestApp.java
@@ -1,6 +1,7 @@
 package com.sequenceiq.it;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,12 +9,16 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.ApplicationContext;
 import org.testng.TestNG;
 import org.testng.internal.Yaml;
 import org.testng.xml.XmlSuite;
 
+import com.sequenceiq.it.cloudbreak.config.ITProps;
+
 @SpringBootApplication
+@EnableConfigurationProperties(ITProps.class)
 public class IntegrationTestApp implements CommandLineRunner {
     @Value("${integrationtest.testsuite.threadpool.size:3}")
     private int suiteThreadPoolSize;
@@ -24,16 +29,27 @@ public class IntegrationTestApp implements CommandLineRunner {
     @Autowired
     private ApplicationContext applicationContext;
 
+    @Autowired
+    private ITProps itProps;
+
     @Override
     public void run(String... args) throws Exception {
         testng.setSuiteThreadPoolSize(suiteThreadPoolSize);
-//        testng.setTestSuites(Arrays.asList(args));
-        testng.setXmlSuites(loadSuites(args));
+        setupSuites(testng, args);
         testng.setVerbose(2);
         testng.run();
     }
 
-    private List<XmlSuite> loadSuites(String... suitePathes) throws Exception {
+    private void setupSuites(TestNG testng, String... args) throws Exception {
+        List<String> suitePathes = itProps.getTestSuites(args[0]);
+        if (suitePathes != null) {
+            testng.setXmlSuites(loadSuites(suitePathes));
+        } else {
+            testng.setTestSuites(Arrays.asList(args));
+        }
+    }
+
+    private List<XmlSuite> loadSuites(List<String> suitePathes) throws Exception {
         List<XmlSuite> suites = new ArrayList<>();
         for (String suitePath: suitePathes) {
             suites.add(loadSuite(suitePath));
--- integrationtest/src/main/java/com/sequenceiq/it/cloudbreak/config/ITProps.java
+++ integrationtest/src/main/java/com/sequenceiq/it/cloudbreak/config/ITProps.java
@@ -1,5 +1,6 @@
 package com.sequenceiq.it.cloudbreak.config;
 
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
@@ -7,6 +8,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 @ConfigurationProperties(prefix = "integrationtest")
 public class ITProps {
     private Map<String, String> credentialNames;
+    private Map<String, List<String>> testSuites;
 
     public void setCredentialNames(Map<String, String> credentialNames) {
         this.credentialNames = credentialNames;
@@ -18,5 +20,17 @@ public class ITProps {
 
     public String getCredentialName(String cloudProvider) {
         return credentialNames.get(cloudProvider);
+    }
+
+    public void setTestSuites(Map<String, List<String>> testSuites) {
+        this.testSuites = testSuites;
+    }
+
+    public Map<String, List<String>> getTestSuites() {
+        return testSuites;
+    }
+
+    public List<String> getTestSuites(String suitesKey) {
+        return testSuites.get(suitesKey);
     }
 }
